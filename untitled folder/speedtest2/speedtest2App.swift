32e4bb65b27346e916251a265ebe9d5cbaecfdfb
M1-50-mariam <76564921+M1-50-mariam@users.noreply.github.com>
2020-12-25 19:25:38 +0300

    Create speedtest2App.swift

---
+++ untitled folder/speedtest2/speedtest2App.swift
@@ -0,0 +1,17 @@
+//
+//  speedtest2App.swift
+//  speedtest2
+//
+//  Created by Mariam2 on 12/25/20.
+//
+
+import SwiftUI
+
+@main
+struct speedtest2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
